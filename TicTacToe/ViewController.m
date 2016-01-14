9e200017e805d6cb5a570af02b0c85937d83cc88
Kanybek Zhagusaev <zhagusaev@gmail.com>
2016-01-14 15:07:47 -0600

    added buttones

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -9,7 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIButton *buttenOne;
+@property (weak, nonatomic) IBOutlet UIButton *buttenTwo;
+@property (weak, nonatomic) IBOutlet UIButton *buttonThree;
+@property (weak, nonatomic) IBOutlet UIButton *buttonFour;
+@property (weak, nonatomic) IBOutlet UIButton *buttonFive;
+@property (weak, nonatomic) IBOutlet UIButton *buttonSix;
+@property (weak, nonatomic) IBOutlet UIButton *buttonSeven;
+@property (weak, nonatomic) IBOutlet UIButton *buttonEight;
+@property (weak, nonatomic) IBOutlet UIButton *buttonNine;
 @end
 
 @implementation ViewController
